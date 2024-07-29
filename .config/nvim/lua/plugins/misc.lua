7fd037be076a5e849427f8c536d697e07437e338
yetea <yetea.dev@gmail.com>
2024-07-29 12:50:52 +0200

    add package info plugin got npm/yarn/pnpm management

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -128,4 +128,73 @@ return {
 			},
 		},
 	},
+	{
+		"vuki656/package-info.nvim",
+		dependencies = { "MunifTanjim/nui.nvim" },
+		keys = {
+			{
+				"<leader>ns",
+				function()
+					require("package-info").show()
+				end,
+				desc = "Show dependency versions",
+				silent = true,
+				noremap = true,
+			},
+			{
+				"<leader>nc",
+				function()
+					require("package-info").hide()
+				end,
+				desc = "Hide dependency versions",
+				silent = true,
+				noremap = true,
+			},
+			{
+				"<leader>nt",
+				function()
+					require("package-info").toggle()
+				end,
+				desc = "Toggle dependency versions",
+				silent = true,
+				noremap = true,
+			},
+			{
+				"<leader>nu",
+				function()
+					require("package-info").update()
+				end,
+				desc = "Update dependency on the line",
+				silent = true,
+				noremap = true,
+			},
+			{
+				"<leader>nd",
+				function()
+					require("package-info").delete()
+				end,
+				desc = "Delete dependency on the line",
+				silent = true,
+				noremap = true,
+			},
+			{
+				"<leader>ni",
+				function()
+					require("package-info").install()
+				end,
+				desc = "Install a new dependency",
+				silent = true,
+				noremap = true,
+			},
+			{
+				"<leader>np",
+				function()
+					require("package-info").change_version()
+				end,
+				desc = "Install a different dependency version",
+				silent = true,
+				noremap = true,
+			},
+		},
+	},
 }
