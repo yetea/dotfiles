0ab4593842a2069e52494bef8b21defa70c120e7
yetea <yetea.dev@gmail.com>
2024-07-30 10:57:38 +0200

    add better diagnostic settings in lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -49,6 +49,25 @@ return {
 					end,
 				},
 			})
+			-- Diagnostic Settings
+			vim.diagnostic.config({
+				underline = true,
+				update_in_insert = false,
+				virtual_text = {
+					spacing = 4,
+					source = "if_many",
+					prefix = "●",
+				},
+				severity_sort = true,
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = "✘",
+						[vim.diagnostic.severity.WARN] = "▲",
+						[vim.diagnostic.severity.HINT] = "⚑",
+						[vim.diagnostic.severity.INFO] = "»",
+					},
+				},
+			})
 		end,
 	},
 }
