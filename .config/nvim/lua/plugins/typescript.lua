dece7f1dcfdc41d0c925da7cbc90827796c94cd5
yetea <yetea.dev@gmail.com>
2024-07-31 10:37:08 +0200

    add better keymaps

--- .config/nvim/lua/plugins/typescript.lua
+++ .config/nvim/lua/plugins/typescript.lua
@@ -9,14 +9,34 @@ return {
 		"pmizio/typescript-tools.nvim",
 		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 		keys = {
-			{ "<leader>ts", "<cmd>TypescriptOrganizeImports<cr>", desc = "Organize Imports" },
-			{ "<leader>tR", "<cmd>TypescriptRenameFile<cr>", desc = "Rename File" },
-			{ "<leader>tu", "<cmd>TypescriptUpdateProject<cr>", desc = "Update Project" },
-			{ "<leader>tI", "<cmd>TypescriptAddMissingImports<cr>", desc = "Add Missing Imports" },
-			{ "<leader>td", "<cmd>TypescriptRemoveUnused<cr>", desc = "Remove Unused" },
-			{ "<leader>tr", "<cmd>TypescriptRemoveUnused<cr>", desc = "Remove Unused" },
-			{ "<leader>tR", "<cmd>TypescriptRenameFile<cr>", desc = "Rename File" },
+			{ "<leader>toi", "<cmd>TSToolsOrganizeImports<cr>", desc = "Sorts and removes unused imports" },
+			{ "<leader>tsi", "<cmd>TSToolsSortImports<cr>", desc = "Sorts imports" },
+			{ "<leader>tri", "<cmd>TSToolsRemoveUnusedImports<cr>", desc = "Removes unused imports" },
+			{ "<leader>tru", "<cmd>TSToolsRemoveUnused<cr>", desc = "Removes all unused statements" },
+			{
+				"<leader>tami",
+				"<cmd>TSToolsAddMissingImports<cr>",
+				desc = "Adds imports for all statements that lack one and can be imported",
+			},
+			{ "<leader>tfa", "<cmd>TSToolsFixAll<cr>", desc = "Fixes all fixable errors" },
+			{
+				"<leader>tgd",
+				"<cmd>TSToolsGoToSourceDefinition<cr>",
+				desc = "Goes to source definition",
+			},
+			{
+				"<leader>trf",
+				"<cmd>TSToolsRenameFile<cr>",
+				desc = "Allow to rename current file and apply changes to connected files",
+			},
+			{
+				"<leader>tfr",
+				"<cmd>TSToolsFileReferences<cr>",
+				desc = "Find files that reference the current file",
+			},
 		},
-		opts = {},
+		init = function()
+			require("typescript-tools").setup({})
+		end,
 	},
 }
