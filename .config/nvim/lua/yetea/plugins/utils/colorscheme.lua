ef7b31692aee2f1d2c35253347db0b9b228bd676
yetea <yetea.dev@gmail.com>
2024-08-05 11:46:09 +0200

    setup nordic theme

--- .config/nvim/lua/yetea/plugins/utils/colorscheme.lua
+++ .config/nvim/lua/yetea/plugins/utils/colorscheme.lua
@@ -4,7 +4,12 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function()
-			require("nordic").load()
+			require("nordic").setup({
+				bold_keywords = true,
+				bright_border = true,
+				reduce_blue = true,
+			})
+			vim.cmd("colorscheme nordic")
 		end,
 	},
 	-- {
