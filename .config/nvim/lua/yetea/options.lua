d03bd640fb320637e3760be7ee6468365ee6b476
yetea <yetea.dev@gmail.com>
2024-08-05 11:58:28 +0200

    refactor signcolumn

--- .config/nvim/lua/yetea/options.lua
+++ .config/nvim/lua/yetea/options.lua
@@ -2,6 +2,7 @@ local opt = vim.opt
 
 opt.relativenumber = true
 opt.number = true
+opt.signcolumn = "yes"
 
 opt.tabstop = 2
 opt.shiftwidth = 2
@@ -17,8 +18,6 @@ opt.cursorline = true
 
 opt.termguicolors = true
 opt.background = "dark"
-opt.signcolumn = "yes"
-
 opt.backspace = "indent,eol,start"
 
 opt.clipboard:append("unnamedplus")
