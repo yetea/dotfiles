d6c0c6290689201cf607920d82c7e59211c8aa2e
yetea <yetea.dev@gmail.com>
2024-08-13 14:55:03 +0200

    add code folding

--- .config/nvim/lua/yetea/options.lua
+++ .config/nvim/lua/yetea/options.lua
@@ -45,6 +45,7 @@ opt.hlsearch = true
 opt.incsearch = true
 
 opt.mouse = ""
+
 -- show tab and trailing characters
 opt.list = true
 opt.listchars = { trail = "·", space = "·", lead = "·" }
@@ -54,12 +55,27 @@ opt.winblend = 0
 opt.pumblend = 0
 opt.pumheight = 10
 
--- Code folding
-opt.foldenable = true
-opt.foldlevel = 99
-opt.foldlevelstart = 99
+-- Folding
+function _G.custom_fold_text()
+  local line_start = vim.fn.getline(vim.v.foldstart)
+  local line_end = vim.fn.getline(vim.v.foldend)
+  local line_count = vim.v.foldend - vim.v.foldstart + 1
+  local total_lines = vim.fn.line('$')
+  local suffix = (" ...  %d %d%% "):format(line_count, line_count / total_lines * 100)
+  local newline = vim.api.nvim_replace_termcodes("<CR>", true, true, true)
+
+  return table.concat({ line_start .. suffix, line_end }, newline)
+end
+
 opt.foldmethod = "expr"
-opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldtext = 'v:lua.custom_fold_text()'
+opt.foldlevel = 99
+opt.foldlevelstart = 1
+opt.fillchars = { eob = "-", fold = " " }
+opt.foldenable = true
+opt.foldnestmax = 3
+opt.foldminlines = 1
 
 -- Automatically remove trailing whitespace.
 vim.cmd([[
--- .config/nvim/lua/yetea/plugins/lang/treesitter.lua
+++ .config/nvim/lua/yetea/plugins/lang/treesitter.lua
@@ -1,45 +1,43 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		dependencies = {
-			"windwp/nvim-ts-autotag",
-		},
-		opts = {
-			ensure_installed = {
-				"bash",
-				"c",
-				"css",
-				"diff",
-				"dockerfile",
-				"gitignore",
-				"html",
-				"javascript",
-				"json",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"tsx",
-				"typescript",
-				"vim",
-				"vimdoc",
-				"yaml",
-			},
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
-			indent = { enable = true },
-			autotag = {
-				enable = true,
-			},
-		},
-		config = function(_, opts)
-			require("nvim-treesitter.install").prefer_git = true
-			---@diagnostic disable-next-line: missing-fields
-			require("nvim-treesitter.configs").setup(opts)
-		end,
-	},
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
+  opts = {
+    ensure_installed = {
+      "bash",
+      "c",
+      "css",
+      "diff",
+      "dockerfile",
+      "gitignore",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "luadoc",
+      "markdown",
+      "markdown_inline",
+      "query",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+    },
+    indent = { enable = true },
+    autotag = {
+      enable = true,
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.install").prefer_git = true
+    ---@diagnostic disable-next-line: missing-fields
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
