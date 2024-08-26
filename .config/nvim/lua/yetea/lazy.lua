2ec78bfec12d3310090c0f06201a3b51a6fdee85
yetea <yetea.dev@gmail.com>
2024-08-26 10:33:01 +0200

    add a few plugins

--- .config/nvim/lua/yetea/lazy.lua
+++ .config/nvim/lua/yetea/lazy.lua
@@ -1,22 +1,23 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { import = "yetea.plugins" },
-  { import = "yetea.plugins.lang" },
-  { import = "yetea.plugins.utils" },
-  { import = "yetea.plugins.git" },
-  { import = "yetea.plugins.ui" },
-  change_detection = { enabled = false, notify = false },
-  ui = { border = "single" },
+	{ import = "yetea.plugins" },
+	{ import = "yetea.plugins.lang" },
+	{ import = "yetea.plugins.utils" },
+	{ import = "yetea.plugins.git" },
+	{ import = "yetea.plugins.ui" },
+	{ import = "yetea.plugins.editor" },
+	change_detection = { enabled = false, notify = false },
+	ui = { border = "single" },
 })
--- .config/nvim/lua/yetea/plugins/lang/treesitter.lua
+++ .config/nvim/lua/yetea/plugins/lang/treesitter.lua
@@ -1,43 +1,42 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  opts = {
-    ensure_installed = {
-      "bash",
-      "c",
-      "css",
-      "diff",
-      "dockerfile",
-      "gitignore",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "luadoc",
-      "markdown",
-      "markdown_inline",
-      "query",
-      "tsx",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "yaml",
-    },
-    auto_install = true,
-    highlight = {
-      enable = true,
-    },
-    indent = { enable = false },
-    autotag = {
-      enable = true,
-    },
-  },
-  config = function(_, opts)
-    require("nvim-treesitter.install").prefer_git = true
-    ---@diagnostic disable-next-line: missing-fields
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"c",
+				"css",
+				"diff",
+				"dockerfile",
+				"gitignore",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"yaml",
+			},
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.install").prefer_git = true
+			---@diagnostic disable-next-line: missing-fields
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		opts = {},
+	},
 }
